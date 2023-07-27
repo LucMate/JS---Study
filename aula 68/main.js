5232feafa83f7471c9baae81aff36468281d53df
Lucas Campos <63018686+LucMate@users.noreply.github.com>
2023-07-27 13:50:52 -0300

    Reduce, filter, map methods
    Signed-off-by: Lucas Campos <63018686+LucMate@users.noreply.github.com>

---
+++ aula 68/main.js
@@ -0,0 +1,17 @@
+//RETORNE O DOBRO DE TODOS OS PARES
+// -> FILTRAR PARES
+// -> DOBRAR OS VALORES
+// -> REDUZIR [SOMAR TUDO]
+
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+const numerosPares = numeros
+.filter(valor =>  valor % 2 === 0)
+.map(valor => valor * 2)
+.reduce((ac, valor) => ac + valor);
+
+
+
+// [ 50, 80,  2, 8,  22 ] pares 
+//[ 100, 160, 4, 16, 44 ] dobro dos pares
+// [324]
+console.log(numerosPares); 
